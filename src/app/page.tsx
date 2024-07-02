bd7be55b78d3b85c50fa6a7d4bfc82c284389afc
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2024-07-02 22:51:46 +0800

    Add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@nextui-org/react";
+import Link from "next/link";
 import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="text-3xl">
       <Button
+        as={Link}
+        href="/members"
         color="primary"
         variant="bordered"
         startContent={<FaRegSmile size={20} />}
